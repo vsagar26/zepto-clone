80b20ad10cada45c48c5cf1f7e97c80bb42f9bde
unknown <vsagar267@gmai;.com>
2023-03-29 22:37:16 +0530

    all cat ui improved

--- src/Pages/AllProducts.jsx
+++ src/Pages/AllProducts.jsx
@@ -1,110 +1,125 @@
 import React, { useEffect, useState } from "react";
 import AllCategories from "../components/AllCategories";
 import ProductDetails from "../components/ProductDetails";
-import { useDispatch, useSelector } from 'react-redux'
+import { useDispatch, useSelector } from "react-redux";
 import { getProducts } from "../Redux/Products/products.actions";
 import { useParams, useSearchParams } from "react-router-dom";
 import axios from "axios";
 import Filter from "../components/Filter";
 
 function AllProducts() {
-  
   const dispatch = useDispatch();
 
-  const {products} = useParams();
+  const { products } = useParams();
   // console.log(products)
-  const [searchParams,setSearchParams] = useSearchParams();
+  const [searchParams, setSearchParams] = useSearchParams();
 
-  const [page,setPage] = useState(searchParams.get("page") || 1)
-  const [sort,setSort] = useState(searchParams.get("sort") || "") 
-  const [totalCount,setTotalCount] = useState(0);
-  
-  
+  const [page, setPage] = useState(searchParams.get("page") || 1);
+  const [sort, setSort] = useState(searchParams.get("sort") || "");
+  const [totalCount, setTotalCount] = useState(0);
+
   const isLoading = useSelector((store) => {
     return store.productReducer.isLoading;
-  })
+  });
 
   const prod = useSelector((store) => {
     return store.productReducer.products;
-  })
-  
-  
-  const getDataOnce = async() => {
-    let res = await axios.get("https://zeptojson.onrender.com/Fruits")
-    let data = res.data;
-    setTotalCount(data.length)
-  }
-  
+  });
 
-  console.log(totalCount)
-  
+  const getDataOnce = async () => {
+    let res = await axios.get("https://zeptojson.onrender.com/Fruits");
+    let data = res.data;
+    setTotalCount(data.length);
+  };
+
+  console.log(totalCount);
 
   let params = {};
-  if(sort) params.sort = sort;
-  if(page) params.page = page;
-  
+  if (sort) params.sort = sort;
+  if (page) params.page = page;
+
   params.limit = 15;
   useEffect(() => {
-    getDataOnce()
-    setSearchParams(params)
+    getDataOnce();
+    setSearchParams(params);
     const getProductParams = {
-      params: { _limit: 15, _page:page}
-    }
-    if(sort) {
+      params: { _limit: 15, _page: page },
+    };
+    if (sort) {
       getProductParams.params._sort = "price";
       getProductParams.params._order = sort;
     }
-    dispatch(getProducts(products,getProductParams))
-    
-  }, [products,sort,page]);
+    dispatch(getProducts(products, getProductParams));
+  }, [products, sort, page]);
 
-  
   const handleSort = (e) => {
     console.log(e.target.value);
     setSort(e.target.value);
-  }
-
+  };
 
   // console.log(sort,"sagar");
   return (
-    <>
-    <div>
-      <select onChange={handleSort} value={sort}>
-        <option value="asc" >Price: Low to High</option>
-        <option value="desc" >Price: High to Low</option>
-      </select>
-    </div>
-    <div className="flex flex-row w-auto">
-      {
-        isLoading ? <div>Loading</div>
-        :
-        <>
-        <div>
-          <AllCategories />
-          <Filter />
-        </div>
-        <div>{products} ({totalCount})</div>
-        <ProductDetails data={prod} />
-        </>
-      }
-    </div>
-    <div>
-      <div>
-        {
-          [...Array(Math.ceil(totalCount / 15)).keys()].map((item) => {
-            return (
-              <button key={item+1} onClick={() => {
-                setPage(item + 1);
-              }}>
-              {item + 1}
-              </button>
-            )
-          })
-        }
+    <div className="">
+      <div className="flex flex-row w-auto">
+        {isLoading ? (
+          <div>Loading..</div>
+        ) : (
+          <div className="flex flex-row ">
+            <div>
+              <AllCategories />
+              <Filter />
+            </div>
+            <div className="pt-5 flex flex-col justify-center items-center">
+              <div className="flex flex-row gap-x-[890px]">
+                <div className="text-2xl font-semibold pb-[20px] ml-6">
+                  {products} ({totalCount})
+                </div>
+                <div>
+                  <select onChange={handleSort} value={sort}>
+                    <option value="asc">Price: Low to High</option>
+                    <option value="desc">Price: High to Low</option>
+                  </select>
+                </div>
+              </div>
+              <div>
+                <ProductDetails data={prod} />
+              </div>
+              <div className="ml-[-100px]">
+                {[...Array(Math.ceil(totalCount / 15)).keys()].map((item) => {
+                  return (
+                    <button
+                      key={item + 1}
+                      onClick={() => {
+                        setPage(item + 1);
+                      }}
+                    className="bg-white border-2 border-[#F61571] hover:bg-[#F61571] px-3 py-2  hover:text-white rounded-lg ml-1 text-[#F61571]">
+                      {item + 1}
+                    </button>
+                  );
+                })}
+              </div>
+              <div></div>
+            </div>
+          </div>
+        )}
       </div>
+      {/* <div>
+        <div>
+          {[...Array(Math.ceil(totalCount / 15)).keys()].map((item) => {
+            return (
+              <button
+                key={item + 1}
+                onClick={() => {
+                  setPage(item + 1);
+                }}
+              >
+                {item + 1}
+              </button>
+            );
+          })}
+        </div>
+      </div> */}
     </div>
-    </>
-    
   );
 }
 
